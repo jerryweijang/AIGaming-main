166a7bba75dd5a2fa6658bc6d35fbdef1cdc8197
Jerry Chen <jerry.chen@ecs.com.tw>
2025-03-19 10:47:11 +0800

    將伺服器埠號改為使用環境變數設定

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;  // 改為環境變數
 
 // Serve static files
 app.use(express.static('./'));
